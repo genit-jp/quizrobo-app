3c5477a847727ac44dd4cf90b9fd164cc8f368ad
Harena Toyokawa <harena.toyokawa@genit.jp>
2025-07-18 10:47:11 +0900

    とりあえずロボットをチャプターに乗せた

--- Assets/Scripts/Select/SelectChallengeStagePanel.cs
+++ Assets/Scripts/Select/SelectChallengeStagePanel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Genit;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,6 +14,7 @@ public class SelectChallengeStagePanel : MonoBehaviour
     private Action _onStartGame;
     private ChapterData[] _chapters;
     private string _subject;
+    private ChapterStar _currentChapterStar;
     
     private void Start()
     {
@@ -54,7 +56,8 @@ public class SelectChallengeStagePanel : MonoBehaviour
         {
             Destroy(child.gameObject); // 既存のボタンを削除
         }
-        
+
+        int currentChapter = 2;
         int chapterCount = _chapters.Length;
         
         float offsetX = 200f;            // 左右のずれ幅
@@ -65,7 +68,7 @@ public class SelectChallengeStagePanel : MonoBehaviour
             var chapterData = _chapters[i];
             var chapterButton = Instantiate(Resources.Load<ChapterStar>("Prefabs/Select/SubjectSelect/ChapterStar"), chapterButtonParent);
             chapterButton.Setup(chapterData, ShowChallengeDialog, _subject);
-
+            
             var rect = chapterButton.GetComponent<RectTransform>();
             rect.anchorMin = rect.anchorMax = new Vector2(0.5f, 1f);
             rect.pivot = new Vector2(0.5f, 1f);
@@ -76,7 +79,13 @@ public class SelectChallengeStagePanel : MonoBehaviour
             float x = (i % 2 == 0) ? offsetX : -offsetX;
 
             rect.anchoredPosition = new Vector2(x, -y);
+            
+            if (chapterData.chapterNumber == currentChapter)
+            {
+                _currentChapterStar = chapterButton;
+            }
         }
+        PlaceRobotOnChapter();
     }
     
     public void OnClickLevelChangeButton()
@@ -109,6 +118,46 @@ public class SelectChallengeStagePanel : MonoBehaviour
                 _onStartGame();
             }
         }, CommonDialog.Mode.OK_CANCEL);
+    }
+
+    private async void PlaceRobotOnChapter()
+    {
+        if (_currentChapterStar == null) return;
+        
+        // ロボットプレハブを生成
+        var roboPrefab = await Utils.InstantiatePrefab("Prefabs/Robo/RoboPrefab", chapterButtonParent);
+        var roboRect = roboPrefab.GetComponent<RectTransform>();
+        
+        // 現在のチャプターボタンの位置を取得
+        var chapterRect = _currentChapterStar.GetComponent<RectTransform>();
+        
+        // ロボットをチャプターボタンの上に配置
+        roboRect.anchorMin = roboRect.anchorMax = new Vector2(0.5f, 1f);
+        roboRect.pivot = new Vector2(0.5f, 0.5f); // 下部中央を基準点に
+        
+        // チャプターボタンからY=-50の位置に配置
+        roboRect.anchoredPosition = new Vector2(
+            chapterRect.anchoredPosition.x, 
+            chapterRect.anchoredPosition.y - 50f
+        );
+        
+        // ロボットのサイズを調整（必要に応じて）
+        roboRect.localScale = new Vector3(0.5f, 0.5f, 1f);
+        
+        // ユーザーの選択したロボットデータを設定
+        var userDataManager = UserDataManager.GetInstance();
+        var userData = userDataManager.GetUserData();
+        var selectedRoboId = userData.selectedRoboId ?? "default";
+        
+        var roboCustomDataDict = userDataManager.GetRoboCustomData(selectedRoboId);
+        if (roboCustomDataDict != null && roboCustomDataDict.ContainsKey(selectedRoboId))
+        {
+            var roboPrefabComponent = roboPrefab.GetComponent<RoboPrefab>();
+            if (roboPrefabComponent != null)
+            {
+                roboPrefabComponent.SetRobo(roboCustomDataDict[selectedRoboId]);
+            }
+        }
     }
     
     public void OnClickCloseButton()
