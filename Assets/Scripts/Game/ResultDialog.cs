59650ac71ca829efb3d0cb47cfb2f7d07596b727
Harena Toyokawa <harena.toyokawa@genit.jp>
2025-07-24 11:49:15 +0900

    リザルトダイアログ修正

--- Assets/Scripts/Game/ResultDialog.cs
+++ Assets/Scripts/Game/ResultDialog.cs
@@ -6,10 +6,13 @@ using UnityEngine.Serialization;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 using Image = UnityEngine.UI.Image;
+using Slider = UnityEngine.UI.Slider;
 
 public class ResultDialog: DialogBaseListener
 {
     [SerializeField] private GameObject _scrollViewContent, _roboContent;
+    [SerializeField] private Slider expSlider;
+    [SerializeField] private Text levelText;
     private Action _onOkButtonClicked;
     
     public async void Setup(List<QuizResultData> quizResults, Action onOkButtonClicked)
@@ -32,6 +35,26 @@ public class ResultDialog: DialogBaseListener
             var roboCustomData = roboCustomDataDict[userDataManager.GetUserData().selectedRoboId];
             await RoboSettingManager.DisplayRobo(_roboContent, roboCustomData);
         }
+        
+        // プレイヤーステータスを表示
+        var playerStatus = userDataManager.GetPlayerStatus();
+        int currentExp = playerStatus.exp;
+        int level = LevelingSystem.CalculateLevelFromExp(currentExp);
+        
+        // 現在のレベルまでに必要だったEXPを計算
+        int expToCurrentLevel = 0;
+        for (int i = 1; i < level; i++)
+        {
+            expToCurrentLevel += LevelingSystem.GetExpToLevelUp(i);
+        }
+        
+        // 次のレベルまでに必要なEXP
+        int expForNextLevel = LevelingSystem.GetExpToLevelUp(level);
+        int expInCurrentLevel = currentExp - expToCurrentLevel;
+        
+        // UIを更新
+        levelText.text = level.ToString();
+        expSlider.value = (float)expInCurrentLevel / expForNextLevel;
         
         _onOkButtonClicked = onOkButtonClicked;
     }
