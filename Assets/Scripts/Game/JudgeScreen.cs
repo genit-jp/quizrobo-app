243a69d5aa1f2da5ad33e02c5fc880798c83d436
Harena Toyokawa <harena.toyokawa@genit.jp>
2025-08-04 16:27:00 +0900

    正解不正解のパネル自動で閉じる・未クリアの文言変更

--- Assets/Scripts/Game/JudgeScreen.cs
+++ Assets/Scripts/Game/JudgeScreen.cs
@@ -1,46 +1,34 @@
 using System;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine.UI;
 
-public class JudgeScreen : MonoBehaviour, IPointerClickHandler
+public class JudgeScreen : MonoBehaviour
 {
-    [SerializeField]private Image _image;
-    [SerializeField]private GameObject _explanationPanel;
-    [SerializeField]private RubyTextMeshProUGUI _ansText;
-    private Action _onClickNextButton;
-    
-    public async void Setup(bool isCorrect,QuizData quizData, Action onClickNextButton)
-    {
-        if (isCorrect)
-        {
-            _ansText.color = new Color(94f/255f, 148f/255f, 232f/255f);
-        }
-        else
-        {
-            _ansText.color = new Color(227f/255f, 101f/255f, 85f/255f);
-        }
-        _ansText.uneditedText = quizData.answer;
-        
+    [SerializeField] private Image _image;
+    [SerializeField] private GameObject _explanationPanel;
+    [SerializeField] private RubyTextMeshProUGUI _ansText;
 
-        _onClickNextButton = onClickNextButton;
-        
-        Texture2D judgeTexture;
-        if (isCorrect)
-        {
-            judgeTexture =  Resources.Load<Texture2D>("Images/maru");
-        }
-        else
-        {
-            judgeTexture =  Resources.Load<Texture2D>("Images/batsu");
-        }
-        _image.sprite = Sprite.Create(judgeTexture, new Rect(0, 0, judgeTexture.width, judgeTexture.height), new Vector2(0.5f, 0.5f));
-    }
-    
-    public async void OnPointerClick(PointerEventData eventData)
+    private Action _onClickNextButton;
+
+    public async void Setup(bool isCorrect, QuizData quizData, Action onClickNextButton)
     {
+        _onClickNextButton = onClickNextButton;
+
+        _ansText.color = isCorrect
+            ? new Color(94f / 255f, 148f / 255f, 232f / 255f)
+            : new Color(227f / 255f, 101f / 255f, 85f / 255f);
+
+        _ansText.uneditedText = quizData.answer;
+
+        var judgeTexture = Resources.Load<Texture2D>(isCorrect ? "Images/maru" : "Images/batsu");
+        _image.sprite = Sprite.Create(judgeTexture, new Rect(0, 0, judgeTexture.width, judgeTexture.height), new Vector2(0.5f, 0.5f));
+
+        // ⏱ 0.5秒後に自動で閉じる
+        await UniTask.Delay(TimeSpan.FromSeconds(0.5f));
         _onClickNextButton?.Invoke();
     }
 }
