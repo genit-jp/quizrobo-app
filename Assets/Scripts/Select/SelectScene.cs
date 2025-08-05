bd8526c86b9d5b87280e56c39e6aa03ad8120562
Harena Toyokawa <harena.toyokawa@genit.jp>
2025-08-05 10:50:58 +0900

    selectSceneにステージ選択画面を移動してきた。

--- Assets/Scripts/Select/SelectScene.cs
+++ Assets/Scripts/Select/SelectScene.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using Genit;
 using TMPro;
 using UnityEngine;
@@ -8,10 +11,10 @@ using UnityEngine.UI;
 public class SelectScene : MonoBehaviour
 {
     [SerializeField] private GameLoadingPanel gameLoadingScene;
-
-    [SerializeField] private GameObject _blocker, roboContainer;
-    [SerializeField] private Slider expSlider;
-    [SerializeField] private Text levelText;
+    [SerializeField] private Transform chapterButtonParent;
+    
+    private ChapterStar _currentChapterStar;
+    
 
     private int _selectedGrade;
     private TimeDispatcher _timer;
@@ -41,11 +44,15 @@ public class SelectScene : MonoBehaviour
                     () => { SceneManager.LoadScene("TitleScene"); });
             }
         };
+        
+        SetChapterButtons();
+        
     }
 
     private async void OnEnable()
     {
         UserDataManager.GetInstance().AddUserDataUpdateListener(UpdatePlayerStatusUI);
+        UserDataManager.GetInstance().AddUserDataUpdateListener(OnUserDataUpdated);
 
         var lastLoginDateTime = Utils.UnixTimeToDateTime(UserDataManager.GetInstance().GetUserData().lastLoginDateTime);
         var now = Clock.GetInstance().Now();
@@ -68,33 +75,13 @@ public class SelectScene : MonoBehaviour
             loginDialog.Setup();
         }
 
-        _blocker.SetActive(false);
-        
-        var userDataManager = UserDataManager.GetInstance();
-        var userData = userDataManager.GetUserData();
-        var selectedRoboId = userData.selectedRoboId ?? "default";
-        
-        var roboCustomDataDict = userDataManager.GetRoboCustomData(selectedRoboId);
-        if (roboCustomDataDict != null && roboCustomDataDict.ContainsKey(selectedRoboId))
-        {
-            var original = roboCustomDataDict[selectedRoboId];
-            UserDataManager.RoboCustomData roboData = new UserDataManager.RoboCustomData
-            {
-                headId = original.headId,
-                bodyId = original.bodyId,
-                armsId = original.armsId,
-                legsId = original.legsId,
-                tailId = original.tailId
-            };
-            await RoboSettingManager.DisplayRobo(roboContainer, roboData);
-        }
-
         UpdatePlayerStatusUI();
     }
 
     private void OnDisable()
     {
         UserDataManager.GetInstance().RemoveUserDataUpdateListener(UpdatePlayerStatusUI);
+        UserDataManager.GetInstance().RemoveUserDataUpdateListener(OnUserDataUpdated);
     }
 
     private void UpdatePlayerStatusUI()
@@ -111,27 +98,165 @@ public class SelectScene : MonoBehaviour
 
         int expForNextLevel = LevelingSystem.GetExpToLevelUp(level);
         int expInCurrentLevel = currentExp - expToCurrentLevel;
-
-        levelText.text = level.ToString();
-        expSlider.value = (float)expInCurrentLevel / expForNextLevel;
+        
     }
-
     
-    public async void OnClickSubjectSelectPanelButton()
+    private void OnUserDataUpdated()
     {
-        var selectSubject = await Utils.InstantiatePrefab("Prefabs/Select/SubjectSelect/SubjectSelectPanel", this.transform);
-        var selectSubjectPanel = selectSubject.GetComponent<SubjectSelectPanel>();
-        selectSubjectPanel.Setup();
-        selectSubjectPanel.StartGame = StartGame;
-        // QuizSelectManager.GetInstance().SetSelectQuizzes(_selectedGrade, 10, Const.PlayMode.Normal);
+        foreach (Transform child in chapterButtonParent)
+        {
+            Destroy(child.gameObject);
+        }
+        SetChapterButtons();
     }
 
-    // public async void OnClickRankingButton()
-    // {
-    //     var rankingDialogObj = await Utils.OpenDialog("Prefabs/Select/RankingDialog", transform);
-    //     var rankingDialog = rankingDialogObj.GetComponent<RankingDialog>();
-    //     rankingDialog.Setup();
-    // }
+    private async void SetChapterButtons()
+    {
+        foreach (Transform child in chapterButtonParent)
+        {
+            Destroy(child.gameObject); // 既存のボタンを削除
+        }
+
+        // StartPoint を配置
+        var startPointObj = await Utils.InstantiatePrefab("Prefabs/Select/SubjectSelect/StartPoint", chapterButtonParent);
+        var startPointRect = startPointObj.GetComponent<RectTransform>();
+        startPointRect.anchorMin = startPointRect.anchorMax = new Vector2(0.5f, 1f);
+        startPointRect.pivot = new Vector2(0.5f, 1f);
+        startPointRect.anchoredPosition = new Vector2(0, -300f);
+
+        // enemy_data.txt を読み込み
+        var enemyDataText = Resources.Load<TextAsset>("Data/enemy_data");
+        if (enemyDataText == null)
+        {
+            Debug.LogError("enemy_data.txt not found");
+            return;
+        }
+
+        string[] lines = enemyDataText.text.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        int chapterCount = lines.Length;
+        int maxClearedChapterNumber = UserDataManager.GetInstance().GetMaxChapterNumber("算数");
+
+        float offsetX = 150f;
+        float verticalOffset = 450f;
+
+        for (int i = 0; i < chapterCount; i++)
+        {
+            int chapterNumber = i + 1;
+
+            var chapterButton = Instantiate(Resources.Load<ChapterStar>("Prefabs/Select/SubjectSelect/ChapterStar"), chapterButtonParent);
+            
+            // chapterData を自前で構築（subject は固定で "算数"）
+            ChapterData chapterData = new ChapterData
+            {
+                chapterNumber = chapterNumber,
+                subject = "算数"
+            };
+
+            chapterButton.Setup(chapterData, ShowChallengeDialog, "算数");
+
+            var rect = chapterButton.GetComponent<RectTransform>();
+            rect.anchorMin = rect.anchorMax = new Vector2(0.5f, 1f);
+            rect.pivot = new Vector2(0.5f, 1f);
+
+            float buttonHeight = rect.sizeDelta.y * 0.7f;
+            float y = verticalOffset + i * buttonHeight;
+            float x = (i % 2 == 0) ? offsetX : -offsetX;
+
+            rect.anchoredPosition = new Vector2(x, -y);
+
+            if (chapterNumber == maxClearedChapterNumber)
+            {
+                _currentChapterStar = chapterButton;
+            }
+        }
+
+        PlaceRobotOnChapter();
+    }
+    
+    private async void ShowChallengeDialog(ChapterData data)
+    {
+        
+        var go = await Genit.Utils.OpenDialog("Prefabs/Common/CommonDialog", this.gameObject.transform);
+        var cd = go.GetComponent<CommonDialog>();
+        var challengeTitle = $"{data.chapterNumber}にチャレンジする？";
+        cd.Setup(challengeTitle, challengeTitle, async result => 
+        {
+            if (result == CommonDialog.Result.OK)
+            {
+                Const.GameSceneParam.Subject = data.subject;
+                Const.GameSceneParam.DifficultyLevel = data.difficultyLevel;
+                Const.GameSceneParam.ChapterNumber = data.chapterNumber;
+                
+                // ダイアログのアニメーションが完了するのを待つ
+                await UniTask.Delay(150);
+                
+                StartGame();
+            }
+        }, CommonDialog.Mode.OK_CANCEL);
+    }
+
+    private async void PlaceRobotOnChapter()
+    {
+        RectTransform targetRect = null;
+        float adjustedY = 0f;
+
+        if (_currentChapterStar != null)
+        {
+            targetRect = _currentChapterStar.GetComponent<RectTransform>();
+            adjustedY = 100f;
+        }
+        else
+        {
+            // StartPoint にロボを配置（最初に生成されていると仮定）
+            foreach (Transform child in chapterButtonParent)
+            {
+                if (child.name.Contains("StartPoint"))
+                {
+                    targetRect = child.GetComponent<RectTransform>();
+                    adjustedY = 0f;
+                    break;
+                }
+            }
+
+            if (targetRect == null)
+            {
+                Debug.LogWarning("StartPoint が見つかりませんでした");
+                return;
+            }
+        }
+
+        // ロボットプレハブを生成
+        var roboPrefab = await Utils.InstantiatePrefab("Prefabs/Robo/RoboPrefab", chapterButtonParent);
+        var roboRect = roboPrefab.GetComponent<RectTransform>();
+
+        // アンカー・ピボット設定
+        roboRect.anchorMin = roboRect.anchorMax = new Vector2(0.5f, 1f);
+        roboRect.pivot = new Vector2(0.5f, 0.5f); // 下部中央を基準点に
+
+        // ロボットの位置を targetRect の下に配置
+        roboRect.anchoredPosition = new Vector2(
+            targetRect.anchoredPosition.x,
+            targetRect.anchoredPosition.y - adjustedY
+        );
+
+        // ロボットのサイズを調整（必要に応じて）
+        roboRect.localScale = new Vector3(0.5f, 0.5f, 1f);
+
+        // ユーザーの選択したロボットデータを設定
+        var userDataManager = UserDataManager.GetInstance();
+        var userData = userDataManager.GetUserData();
+        var selectedRoboId = userData.selectedRoboId ?? "default";
+
+        var roboCustomDataDict = userDataManager.GetRoboCustomData(selectedRoboId);
+        if (roboCustomDataDict != null && roboCustomDataDict.ContainsKey(selectedRoboId))
+        {
+            var roboPrefabComponent = roboPrefab.GetComponent<RoboPrefab>();
+            if (roboPrefabComponent != null)
+            {
+                roboPrefabComponent.SetRobo(roboCustomDataDict[selectedRoboId]);
+            }
+        }
+    }
 
     public void OnTappedGoToCustomSceneButton()
     {
--- Assets/Scripts/Select/SubjectSelectPanel.cs
+++ Assets/Scripts/Select/SubjectSelectPanel.cs
@@ -1,70 +1,70 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Cysharp.Threading.Tasks;
-using Genit;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class SubjectSelectPanel : MonoBehaviour
-{
-    [SerializeField] private Transform buttonContainer;
-
-    public Action StartGame;
-
-    public async void Setup()
-    {
-        foreach (Transform child in buttonContainer)
-        {
-            Destroy(child.gameObject); 
-        }
-
-        var subjects = MasterData.GetInstance().AllSubjects();
-        int buttonCount = 5;
-
-        var containerRect = buttonContainer.GetComponent<RectTransform>().rect;
-        float containerHeight = containerRect.height;
-        float containerWidth = containerRect.width;
-
-        float amplitude = (containerWidth - 100) / 4f;
-        float wavelength = (containerHeight * 2f - 400) / 5f ; 
-        
-        float headOffset = 100f;
-
-        for (int i = 0; i < buttonCount; i++)
-        {
-            var subject = subjects[i];
-            var subjectButton = await Utils.InstantiatePrefab("Prefabs/Select/SubjectSelect/SubjectButton", buttonContainer);
-            var btn = subjectButton.GetComponent<SubjectButton>();
-            btn.Setup(subject, () => this.OnClickSubjectButton(subject));
-
-            var rect = btn.GetComponent<RectTransform>();
-            rect.anchorMin = new Vector2(0.5f, 1f);
-            rect.anchorMax = new Vector2(0.5f, 1f);
-            rect.pivot = new Vector2(0.5f, 0.5f);
-
-            float angle = (2 * i + 1) * Mathf.PI / 2f;
-            float y = angle * (wavelength / (2f * Mathf.PI))　+ headOffset;
-            float x = Mathf.Sin(angle) * amplitude;
-
-            rect.anchoredPosition = new Vector2(x, -y);
-        }
-    }
-
-
-    private async void OnClickSubjectButton(string subject)
-    {
-        var selectPanel = await Utils.InstantiatePrefab("Prefabs/Select/SubjectSelect/SelectChallengeStagePanel", this.gameObject.transform);
-        var selectChallengeStagePanel = selectPanel.GetComponent<SelectChallengeStagePanel>();
-        selectChallengeStagePanel.Setup(subject, () =>
-        {
-            Debug.Log("StartGame呼び出し");
-            StartGame();
-        });
-    }
-
-    public void OnClickGoBackButton()
-    {
-        Destroy(this.gameObject);
-    }
-}
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using Cysharp.Threading.Tasks;
+// using Genit;
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
+//
+// public class SubjectSelectPanel : MonoBehaviour
+// {
+//     [SerializeField] private Transform buttonContainer;
+//
+//     public Action StartGame;
+//
+//     public async void Setup()
+//     {
+//         foreach (Transform child in buttonContainer)
+//         {
+//             Destroy(child.gameObject); 
+//         }
+//
+//         var subjects = MasterData.GetInstance().AllSubjects();
+//         int buttonCount = 5;
+//
+//         var containerRect = buttonContainer.GetComponent<RectTransform>().rect;
+//         float containerHeight = containerRect.height;
+//         float containerWidth = containerRect.width;
+//
+//         float amplitude = (containerWidth - 100) / 4f;
+//         float wavelength = (containerHeight * 2f - 400) / 5f ; 
+//         
+//         float headOffset = 100f;
+//
+//         for (int i = 0; i < buttonCount; i++)
+//         {
+//             var subject = subjects[i];
+//             var subjectButton = await Utils.InstantiatePrefab("Prefabs/Select/SubjectSelect/SubjectButton", buttonContainer);
+//             var btn = subjectButton.GetComponent<SubjectButton>();
+//             btn.Setup(subject, () => this.OnClickSubjectButton(subject));
+//
+//             var rect = btn.GetComponent<RectTransform>();
+//             rect.anchorMin = new Vector2(0.5f, 1f);
+//             rect.anchorMax = new Vector2(0.5f, 1f);
+//             rect.pivot = new Vector2(0.5f, 0.5f);
+//
+//             float angle = (2 * i + 1) * Mathf.PI / 2f;
+//             float y = angle * (wavelength / (2f * Mathf.PI))　+ headOffset;
+//             float x = Mathf.Sin(angle) * amplitude;
+//
+//             rect.anchoredPosition = new Vector2(x, -y);
+//         }
+//     }
+//
+//
+//     private async void OnClickSubjectButton(string subject)
+//     {
+//         var selectPanel = await Utils.InstantiatePrefab("Prefabs/Select/SubjectSelect/SelectChallengeStagePanel", this.gameObject.transform);
+//         var selectChallengeStagePanel = selectPanel.GetComponent<SelectChallengeStagePanel>();
+//         selectChallengeStagePanel.Setup(subject, () =>
+//         {
+//             Debug.Log("StartGame呼び出し");
+//             StartGame();
+//         });
+//     }
+//
+//     public void OnClickGoBackButton()
+//     {
+//         Destroy(this.gameObject);
+//     }
+// }
